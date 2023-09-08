73defa92160c17b164b884374c985df23aafb6b0
Steve Kim <stevekim@Steves-MacBook-Pro.local>
2023-09-07 17:32:33 -0700

    added mutations.js and queries.js to utils

---
+++ server/utils/mutations.js
@@ -0,0 +1,21 @@
+import { gql } from '@apollo/client';
+
+export const ADD_PROFILE = gql`
+  mutation addProfile($name: String!) {
+    addProfile(name: $name) {
+      _id
+      name
+      completedExercises
+    }
+  }
+`;
+
+export const ADD_COMPLETEDEXERCISES = gql`
+  mutation addCompletedExercises($profileId: ID!, $completedExercises: String!) {
+    addCompletedExercises(profileId: $profileId, completedExercises: $completedExercises) {
+      _id
+      name
+      completedExercises
+    }
+  }
+`;
