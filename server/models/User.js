2ed92481171bd755d77a493b83a6398fb61c78bb
Sydney Laub <syd.laub18@gmail.com>
2023-09-11 12:50:25 -0400

    updated user model

--- server/models/User.js
+++ server/models/User.js
@@ -1,12 +1,8 @@
+const shortid = require('shortid')
 const { Schema, model } = require('mongoose');
+const bcrypt = require('bcrypt');
 
-const profileSchema = new Schema({
-  name: {
-    type: String,
-    required: true,
-    unique: true,
-    trim: true,
-  },
+const userSchema = new Schema({
   email: {
     type: String,
     required: true,
@@ -17,11 +13,10 @@ const profileSchema = new Schema({
     type: String,
     required: true,
     minlength: 5,
-  },
+  }
 });
 
-// set up pre-save middleware to create password
-profileSchema.pre('save', async function (next) {
+userSchema.pre('save', async function (next) {
   if (this.isNew || this.isModified('password')) {
     const saltRounds = 10;
     this.password = await bcrypt.hash(this.password, saltRounds);
@@ -30,12 +25,10 @@ profileSchema.pre('save', async function (next) {
   next();
 });
 
-// compare the incoming password with the hashed password
-profileSchema.methods.isCorrectPassword = async function (password) {
+userSchema.methods.isCorrectPassword = async function (password) {
   return bcrypt.compare(password, this.password);
 };
 
+const User = model('User', userSchema);
 
-const Profile = model('Profile', profileSchema);
-
-module.exports = Profile;
+module.exports = User;
