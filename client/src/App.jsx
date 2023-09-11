7535d3a48afdd026192f8dae37f01cfaba496c73
Sydney Laub <syd.laub18@gmail.com>
2023-09-11 12:59:33 -0400

    updated app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,54 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { useAuth0 } from '@auth0/auth0-react';
+import './App.css'
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+import { Outlet } from 'react-router-dom';
 
+import Header from './components/Header';
+import Footer from './components/Footer';
 
-import Login from './components/login';
-import Navigation from './components/navbar';
-import Home from './pages/homePage';
-import Profile from './pages/profilePage';
-import Workouts from './pages/workoutPage';
+// Construct our main GraphQL API endpoint
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
 
-import './App.css';
+// Construct request middleware that will attach the JWT token to every request as an `authorization` header
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = localStorage.getItem('id_token');
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  // Set up our client to execute the `authLink` middleware prior to making the request to our GraphQL API
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
 
 
 function App() {
-  const { isLoading, isAuthenticated } = useAuth0();
-  if (isLoading) return <div>Loading...</div>
 
   return (
-    <>
-      {isAuthenticated && <Navigation />}
-      <Router>
-        {/* <Navigation/> */}
-        <Routes>
-          <Route path='/' element={<Login />} />
-          <Route path='/home' element={<Home />} />
-          <Route path='/profile' element={<Profile />} />
-          <Route path='/workouts' element={<Workouts />} />
-        </Routes>
-      </Router>
-    </>
-
+    <ApolloProvider client={client}>
+      <div>
+        <Header />
+        <div className='container'>
+          <Outlet />
+        </div>
+        <Footer />
+      </div>
+    </ApolloProvider>
   );
 }
 
