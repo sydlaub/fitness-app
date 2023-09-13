ba544ffdeb3c4301d5a52001ceba02f7da1576d6
Steve Kim <stevekim@Steves-MacBook-Pro.local>
2023-09-12 17:13:37 -0700

    added ExerciseCard, ExerciseForm, ExerciseList, ExerciseDetail components, reformatted NewExercise.jsx to call Card and Form components

---
+++ client/src/components/ExerciseDetail.js
@@ -0,0 +1,26 @@
+// ExerciseDetail.js
+import React, { useEffect, useState } from 'react';
+import { fetchData, exerciseOptions } from '../utils/fetchData';
+
+const ExerciseDetail = ({ id }) => {
+    const [exerciseDetail, setExerciseDetail] = useState({});
+
+    useEffect(() => {
+        const fetchExerciseData = async () => {
+            const exerciseDbUrl = 'https://exercisedb.p.rapidapi.com/exercises';
+            const exerciseDetailData = await fetchData(`${exerciseDbUrl}/${id}`, exerciseOptions);
+            setExerciseDetail(exerciseDetailData);
+        };
+
+        fetchExerciseData();
+    }, [id]);
+
+    return (
+        <div>
+            <h3>Exercise Detail</h3>
+            <pre>{JSON.stringify(exerciseDetail, null, 2)}</pre>
+        </div>
+    );
+};
+
+export default ExerciseDetail;
